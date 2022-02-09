17a819d4a41809cef0e81ad871e9e45b2c828da8
Augusto <augusto.bisetto@student.h-is.com>
2022-02-09 12:46:59 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.set("view engine", "ejs")
+
+app.get( '/', (req,res) => {
+    res.render('index')
+})
+
+app.get( '/test', (req,res) => {
+    res.render('test')
+})
+
+app.listen(3000)
+
