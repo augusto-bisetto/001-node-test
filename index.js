abbe1f5e6e4506925af79f009e17e5c6a99e98fd
Augusto <augusto.bisetto@student.h-is.com>
2022-02-14 12:37:51 +0100

    todo

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require('express')
 const app = express()
+const todos = [{
+    id: '38347',
+    message: 'Cook the carbonara',
+    completed: false
+  }]
 
+  
 app.set("view engine", "ejs")
 
 app.get( '/', (req,res) => {
     res.render('index')
 })
 
-app.get( '/test', (req,res) => {
-    res.render('test')
-})
+app.get('/todo/:id', (req,res) => {
+    const id = req.params.id
+  
+    let todo = null
+    let i = 0
+  
+    // find the todo with the same Id
+    while (i < todos.length ){
+      if (todos[i].id == id){
+        todo = todos[i]
+      }
+      i++;
+    }
+    if ( todo ){
+      res.render('todo',{
+        id : id,
+        todo: todo
+      })  
+    }else{
+      res.render('404')
+    }
+  })
 
 app.listen(3000)
 
