edbd09fd3e76a33ce30718121783abcbbb18e747
Augusto <augusto.bisetto@student.h-is.com>
2022-03-02 00:09:20 +0100

    modules.js

--- modules.js
+++ modules.js
@@ -1,1 +1,17 @@
-//idea: a website where you can do exercise, have competion with others with dome rewards
+const user = {
+    id: '',
+    name: Augusto,
+    points: 10,
+    status: online,
+},
+const exercise = [{
+    subject: Math,
+    difficultyLevel: 5,
+    completed: false
+},
+{
+    subject: English,
+    difficultyLevel: 3,
+    completed: false
+}]
+// const bestUser = "user with highest score"
